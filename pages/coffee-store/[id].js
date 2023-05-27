f3129f2f5996fa27c155fc279a3ca3e9942b83b1
Aleem Awan <aleemawan366@outlook.com>
2023-05-27 13:02:19 +0500

    adds store details page

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,115 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+
+
+import cls from "classnames";
+
+import styles from "../../styles/coffee-store.module.css";
+
+import coffeeStores from "@/data/coffee-stores.json";
+
+export async function getStaticProps(staticProps) {
+    const params = staticProps.params;
+
+    const findCoffeeStoreById = coffeeStores.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id; //dynamic id
+    });
+    return {
+        props: {
+            coffeeStore: findCoffeeStoreById ? findCoffeeStoreById : {},
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const paths = coffeeStores.map((coffeeStore) => {
+        return {
+            params: {
+                id: coffeeStore.id.toString(),
+            },
+        };
+    });
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+const CoffeeStore = (props) => {
+    const router = useRouter();
+
+    if (router.isFallback) {
+        return <div>Loading...</div>;
+    }
+
+    const {address,name,neighbourhood} = props.coffeeStore
+
+    return (
+        <div className={styles.layout}>
+            <Head>
+                <title>{name}</title>
+                <meta name="description" content={`${name} coffee store`} />
+            </Head>
+            <div className={styles.container}>
+                <div className={styles.col1}>
+                    <div className={styles.backToHomeLink}>
+                        <Link href="/">← Back to home</Link>
+                    </div>
+                    <div className={styles.nameWrapper}>
+                        <h1 className={styles.name}>{name}</h1>
+                    </div>
+                    <Image
+                        src={
+                            imgUrl ||
+                            "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                        }
+                        width={600}
+                        height={360}
+                        className={styles.storeImg}
+                        alt={name}
+                    />
+                </div>
+
+                <div className={cls("glass", styles.col2)}>
+                    {address && (
+                        <div className={styles.iconWrapper}>
+                            <Image
+                                src="/static/icons/places.svg"
+                                width="24"
+                                height="24"
+                                alt="places icon"
+                            />
+                            <p className={styles.text}>{address}</p>
+                        </div>
+                    )}
+                    {neighbourhood && (
+                        <div className={styles.iconWrapper}>
+                            <Image
+                                src="/static/icons/nearMe.svg"
+                                width="24"
+                                height="24"
+                                alt="near me icon"
+                            />
+                            <p className={styles.text}>{neighbourhood}</p>
+                        </div>
+                    )}
+                    <div className={styles.iconWrapper}>
+                        <Image
+                            src="/static/icons/star.svg"
+                            width="24"
+                            height="24"
+                            alt="star icon"
+                        />
+                        <p className={styles.text}>{0}</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CoffeeStore;
